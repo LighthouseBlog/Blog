c9dd1d36ff5684331ec8d3c748d38afc9c06a478
Sam Pastoriza <sampastoriza@sams-mbp.wlan.rose-hulman.edu>
2017-03-31 20:47:37 -0400

    Added three new components, holy grail layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { AdsComponent } from './ads/ads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    AdsComponent
   ],
   imports: [
     BrowserModule,
