63f86f61036c75226e62c316335b1e509ec636a9
pastorsj <pastorsj@rose-hulman.edu>
2017-05-22 22:18:57 -0400

    Some progress on menu bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  title = "Programmer's Paradise"
+  title = "Programmer's Paradise";
+  opened = true;
+
+  @Output()
+  clicked: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
+
+  onMenuClick() {
+    this.clicked.emit(this.opened);
+  }
 
   ngOnInit() {
   }
