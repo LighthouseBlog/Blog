4fe952a7fe931a87553ef451595b2a591887712b
pastorsj <pastorsj@rose-hulman.edu>
2017-06-13 11:18:36 -0400

    Am now able to get the html from the editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -7,12 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  editing = false;
+  public editing = false;
+  public options: Object = {
+    placeholderText: 'Edit Content Here',
+    charCounterCount: true
+  };
+  private content: Object;
 
   constructor() { }
 
   ngOnInit() {
     this.editing = true;
+    this.options['events'] = {
+      'froalaEditor.contentChanged': (e, editor) => {
+        this.updateContent(editor);
+      }
+    };
+  }
+
+  updateContent(editor) {
+    this.content = editor.html.get();
   }
 
   saveArticle() {
