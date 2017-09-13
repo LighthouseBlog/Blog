2dda79e3f9b074912c59782849c3b21e47b203ed
pastorsj <pastorsj@rose-hulman.edu>
2017-09-12 20:51:16 -0400

    Began progress of writing froala plugin

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -9,6 +9,8 @@ import { ImagesService } from '../_services/images.service';
 
 import { environment } from '../../environments/environment';
 
+declare var $: any;
+
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
@@ -20,6 +22,7 @@ export class EditorComponent implements OnInit {
     heightMin: 400,
     placeholderText: 'Edit Content Here',
     charCounterCount: true,
+    htmlRemoveTags: [],
     events: {
       'froalaEditor.contentChanged': (e, editor) => {
         this.updateContent(editor);
@@ -31,7 +34,13 @@ export class EditorComponent implements OnInit {
             console.log('Result', result);
           })
       }
-    }
+    },
+    toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough',
+    'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle',
+    'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent',
+    'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile',
+    'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll',
+    'clearFormatting', '|', 'print', 'spellChecker', 'help', 'html', '|', 'undo', 'redo', 'github']
   };
   private content: string;
 
@@ -66,7 +75,22 @@ export class EditorComponent implements OnInit {
       });
   }
 
+  initializeGistButton() {
+    $.FroalaEditor.DefineIcon('github', {NAME: 'github'});
+    $.FroalaEditor.RegisterCommand('github', {
+      title: 'Convert URL to GIST Format',
+      focus: false,
+      undo: false,
+      refreshAfterCallback: false,
+
+      callback: function () {
+        alert('Convert here?');
+      }
+    });
+  }
+
   ngOnInit() {
+    this.initializeGistButton();
     this.editing = true;
     this.route.params.subscribe(params => {
       const id = params['id'];
