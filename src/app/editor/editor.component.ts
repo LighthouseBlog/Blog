3e3429124fb628f3d65fe412db0693690a5e63b8
pastorsj <pastorsj@rose-hulman.edu>
2017-09-16 09:55:28 -0400

    Added initial code for custom plugin

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -40,7 +40,8 @@ export class EditorComponent implements OnInit {
     'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent',
     'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile',
     'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll',
-    'clearFormatting', '|', 'print', 'spellChecker', 'help', 'html', '|', 'undo', 'redo', 'github']
+    'clearFormatting', '|', 'print', 'spellChecker', 'help', 'html', '|', 'undo', 'redo', 'github'],
+    pluginsEnabled: ['customPlugin']
   };
   private content: string;
 
@@ -76,15 +77,131 @@ export class EditorComponent implements OnInit {
   }
 
   initializeGistButton() {
-    $.FroalaEditor.DefineIcon('github', {NAME: 'github'});
-    $.FroalaEditor.RegisterCommand('github', {
-      title: 'Convert URL to GIST Format',
-      focus: false,
-      undo: false,
-      refreshAfterCallback: false,
+      // Define popup template.
+    $.extend($.FroalaEditor.POPUP_TEMPLATES, {
+      'customPlugin.popup': '[_BUTTONS_][_CUSTOM_LAYER_]'
+    });
 
+    // Define popup buttons.
+    $.extend($.FroalaEditor.DEFAULTS, {
+      popupButtons: ['popupClose', '|', 'popupButton1', 'popupButton2'],
+    });
+
+    // The custom popup is defined inside a plugin (new or existing).
+    $.FroalaEditor.PLUGINS.customPlugin = function (editor) {
+      // Create custom popup.
+      function initPopup () {
+        // Popup buttons.
+        let popup_buttons = '';
+
+        // Create the list of buttons.
+        if (editor.opts.popupButtons.length > 1) {
+          popup_buttons += '<div class="fr-buttons">';
+          popup_buttons += editor.button.buildList(editor.opts.popupButtons);
+          popup_buttons += '</div>';
+        }
+
+        // Load popup template.
+        const template = {
+          buttons: popup_buttons,
+          custom_layer: `
+          <div>
+              <input mdInput placeholder="Url" value="">
+          </div>`
+        };
+
+        // Create popup.
+        const $popup = editor.popups.create('customPlugin.popup', template);
+
+        return $popup;
+      }
+
+      // Show the popup
+      function showPopup () {
+        // Get the popup object defined above.
+        let $popup = editor.popups.get('customPlugin.popup');
+
+        // If popup doesn't exist then create it.
+        // To improve performance it is best to create the popup when it is first needed
+        // and not when the editor is initialized.
+        if (!$popup) {
+          $popup = initPopup()
+        }
+
+        // Set the editor toolbar as the popup's container.
+        editor.popups.setContainer('customPlugin.popup', editor.$tb);
+
+        // This will trigger the refresh event assigned to the popup.
+        // editor.popups.refresh('customPlugin.popup');
+
+        // This custom popup is opened by pressing a button from the editor's toolbar.
+        // Get the button's object in order to place the popup relative to it.
+        const $btn = editor.$tb.find('.fr-command[data-cmd="github"]');
+
+        // Set the popup's position.
+        const left = $btn.offset().left + $btn.outerWidth() / 2;
+        const top = $btn.offset().top + (editor.opts.toolbarBottom ? 10 : $btn.outerHeight() - 10);
+
+        // Show the custom popup.
+        // The button's outerHeight is required in case the popup needs to be displayed above it.
+        editor.popups.show('customPlugin.popup', left, top, $btn.outerHeight());
+      }
+
+      // Hide the custom popup.
+      function hidePopup () {
+        editor.popups.hide('customPlugin.popup');
+      }
+
+      // Methods visible outside the plugin.
+      return {
+        showPopup: showPopup,
+        hidePopup: hidePopup
+      }
+    }
+
+    // Define an icon and command for the button that opens the custom popup.
+    $.FroalaEditor.DefineIcon('github', { NAME: 'github'})
+    $.FroalaEditor.RegisterCommand('github', {
+      title: 'Convert Url to GISTlike code',
+      icon: 'github',
+      undo: false,
+      focus: false,
+      plugin: 'customPlugin',
       callback: function () {
-        alert('Convert here?');
+        this.customPlugin.showPopup();
+      }
+    });
+
+    // Define custom popup close button icon and command.
+    $.FroalaEditor.DefineIcon('popupClose', { NAME: 'times' });
+    $.FroalaEditor.RegisterCommand('popupClose', {
+      title: 'Close',
+      undo: false,
+      focus: false,
+      callback: function () {
+        this.customPlugin.hidePopup();
+      }
+    });
+
+    // Define custom popup 1.
+    $.FroalaEditor.DefineIcon('popupButton1', { NAME: 'bell-o' });
+    $.FroalaEditor.RegisterCommand('popupButton1', {
+      title: 'Button 1',
+      undo: false,
+      focus: false,
+      callback: function () {
+        alert('popupButton1 was pressed');
+      }
+    });
+
+    // Define custom popup 2.
+    $.FroalaEditor.DefineIcon('popupButton2', { NAME: 'bullhorn' });
+    $.FroalaEditor.RegisterCommand('popupButton2', {
+      title: 'Button 2',
+      undo: false,
+      focus: false,
+      callback: function () {
+        alert('popupButton2');
       }
     });
   }
