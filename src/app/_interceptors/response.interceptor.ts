cbbcd50b6a8c839e1d53049da49c550714503f23
pastorsj <pastorsj@rose-hulman.edu>
2017-11-12 12:59:15 -0500

    Updated response interceptor:

--- src/app/_interceptors/response.interceptor.ts
+++ src/app/_interceptors/response.interceptor.ts
@@ -2,9 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ResponseInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -18,6 +22,9 @@ export class ResponseInterceptor implements HttpInterceptor {
     // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof HttpErrorResponse) {
+      if (error.status === 401) {
+        this.router.navigateByUrl('/');
+      }
       const err = error.error || JSON.stringify(error);
       errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
     } else {
