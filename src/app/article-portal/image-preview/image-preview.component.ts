2bac134cd14a54d31a619c4543c7a935aa4c2a95
pastorsj <pastorsj@rose-hulman.edu>
2017-11-16 21:07:51 -0500

    Got basic cropping to work

--- src/app/article-portal/image-preview/image-preview.component.ts
+++ src/app/article-portal/image-preview/image-preview.component.ts
@@ -1,43 +1,61 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import Cropper from 'cropperjs'
+import { SnackbarMessagingService } from 'app/_services/snackbar-messaging.service';
 
 @Component({
   selector: 'app-image-preview',
   templateUrl: './image-preview.component.html',
   styleUrls: ['./image-preview.component.scss']
 })
-export class ImagePreviewComponent {
+export class ImagePreviewComponent implements OnInit {
 
   private cropper: Cropper;
+  private originalImage: any;
+  public showingCroppingTools: boolean;
 
   constructor(
     public dialogRef: MatDialogRef<ImagePreviewComponent>,
+    private snackbarMessagingService: SnackbarMessagingService,
     @Inject(MAT_DIALOG_DATA) public image: any
   ) {
-    console.log('Image', this.image);
+    this.originalImage = image;
+  }
+
+  ngOnInit() {
+    this.showingCroppingTools = false;
   }
 
   showCropperTool() {
     const image = document.getElementById('image');
-    console.log('Image', image);
     this.cropper = new Cropper(image, {
       aspectRatio: 16 / 9
     });
+    this.showingCroppingTools = true;
   }
 
-  crop() {
-    this.cropper.crop();
-    const data = this.cropper.getData();
-    console.log('Data', data);
-    this.stop();
+  cropImage() {
+    const imageData = this.cropper.getCroppedCanvas().toDataURL('image/png');
+    this.cropper.replace(imageData);
+  }
+
+  save() {
+    const imageData = this.cropper.getCroppedCanvas().toBlob((blob) => {
+      this.stop();
+      this.dialogRef.close(blob);
+    });
   }
 
   stop() {
     if (this.cropper) {
       this.cropper.destroy();
       this.cropper = null;
+      this.showingCroppingTools = false;
     }
+  }
+
+  restore() {
+    this.cropper.replace(this.originalImage);
   }
 
   onNoClick(): void {
