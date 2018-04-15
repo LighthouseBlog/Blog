e92d4e590b873ab6b72b6f20841ac643a9e24a32
pastorsj <sampastoriza@yahoo.com>
2018-04-15 14:42:26 -0400

    Removed unused service

--- src/app/article-portal/user-articles/user-articles.component.ts
+++ src/app/article-portal/user-articles/user-articles.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { CreateArticleModalComponent } from '../create-article-modal/create-article-modal.component';
 
-import { AuthorService } from 'app/_services/author.service';
-
 @Component({
     selector: 'user-articles',
     templateUrl: './user-articles.component.html',
